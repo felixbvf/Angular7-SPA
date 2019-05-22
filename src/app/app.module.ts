53468954fe1dd81d9ab1454d053065e62c67bc7e
Felix Balderrama <felixbvf@gmail.com>
2019-05-22 11:40:14 -0400

    Reset Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; //peticiones http
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,7 +27,8 @@ import {CochesComponent } from './coches/coches.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -1,19 +1,42 @@
 import { Component } from '@angular/core';
 import {Coche } from './coche';
+import {PeticionesService} from '../services/peticiones.service'; //se importa el servicio
 
 @Component({
   selector: 'coches',
-  templateUrl: './coches.component.html'
+  templateUrl: './coches.component.html',
+  providers:  [PeticionesService] //se agrega el servicio
 })
 export class CochesComponent{
   public coche: Coche;
   public coches:Array<Coche>;
-  constructor(){
+  public articulos;
+  constructor(
+    private _peticionesService: PeticionesService
+  ){
     this.coche = new Coche("",0,"");
     this.coches = [
       new Coche("Seat Panda", 120,"Blanco"),
       new Coche("Renault Clio",110, "Azul")
     ];
+  }
+
+  ngOnInit(){
+    //console.log(this._peticionesService.getPrueba());
+    this._peticionesService.getArticulos().subscribe(
+      result => {
+        this.articulos = result;
+        console.log(result);
+        if(!this.articulos){
+          console.log("Error en el servidor");
+        }
+      },
+      error => {
+        var errorMenssage = <any>error;
+        console.log(errorMenssage);
+      }
+    );
+
   }
 
   onSubmit(){
